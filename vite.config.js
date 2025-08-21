b442513eca8dc6b3e613e60b1637a305cbfe28cc
Kristijan Bobovec <boboveckristijan@gmail.com>
2025-08-21 19:29:15 +0200

    Added responsivnes to the website

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path";
 
 export default defineConfig({
   build: {
@@ -12,4 +14,11 @@ export default defineConfig({
       },
     },
   },
+
+  plugins: [
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      symbolId: "icon-[dir]-[name]",
+    }),
+  ],
 });
